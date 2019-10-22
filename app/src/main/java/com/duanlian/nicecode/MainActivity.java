7ef37067f88656d958a15fd946f5b371f64c1790
duanlian <10210950@qq.com>
2019-10-22 09:55:55 +0800

    nice codes

---
+++ app/src/main/java/com/duanlian/nicecode/MainActivity.java
@@ -0,0 +1,14 @@
+package com.duanlian.nicecode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
