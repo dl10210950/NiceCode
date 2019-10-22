9e2179c5c4e0783e8372f632d5146314b90c3313
duanlian <10210950@qq.com>
2019-10-22 11:27:04 +0800

    Code add

---
+++ nicecodes/src/main/java/com/duanlian/nicecodes/base/BaseActivity.java
@@ -0,0 +1,262 @@
+package com.duanlian.nicecodes.base;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.duanlian.nicecodes.widget.CustomConfirmDialog;
+
+
+/**
+ * Activity的基类
+ */
+public abstract class BaseActivity extends FragmentActivity implements IBaseView {
+    private ProgressDialog mProgressDialog;
+    FragmentManager fragmentManager;
+    private CustomConfirmDialog confirmDialog;
+    //private ImmersionBar immersionBar;
+
+    /**
+     * 初始化布局
+     */
+    public abstract int setContentView();
+
+    /**
+     * 初始化控件
+     */
+    public abstract void initView();
+
+    /**
+     * 初始化控制中心
+     */
+   // public abstract void initPresenter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //initPresenter();
+
+//        //初始化沉浸式状态栏
+//        immersionBar = ImmersionBar.with(this);
+//        immersionBar.statusBarColor("#FF581C48")
+//                    .fitsSystemWindows(true)
+//                    .init();
+        setContentView(setContentView());
+//        // 初始化View注入
+//        ButterKnife.bind(this);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+
+    }
+
+
+    /**
+     * 显示单选对话框
+     *
+     * @param title           标题
+     * @param message         提示信息
+     * @param strings         选项数组
+     * @param checkedItem     默认选中
+     * @param onClickListener 点击事件的监听
+     */
+    public void showRadioButtonDialog(String title, String message, String[] strings, int checkedItem, DialogInterface.OnClickListener onClickListener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        if (!TextUtils.isEmpty(message)) {
+            builder.setMessage(message);
+        }
+        builder.setSingleChoiceItems(strings, checkedItem, onClickListener);
+        builder.create();
+        builder.show();
+    }
+
+    /**
+     * 显示单选对话框
+     *
+     * @param title           标题
+     * @param strings         选项数组
+     * @param onClickListener 点击事件的监听
+     */
+    public void showRadioButtonDialog(String title, String[] strings, DialogInterface.OnClickListener onClickListener) {
+        showRadioButtonDialog(title, null, strings, 0, onClickListener);
+    }
+
+    /**
+     * 弹出自定义对话框
+     */
+    public void showConfirmDialog(String title, View.OnClickListener positiveListener) {
+        if (confirmDialog == null) {
+            confirmDialog = new CustomConfirmDialog(this, title, positiveListener);
+        }
+        confirmDialog.show();
+    }
+    /**
+     * 关闭自定义对话框
+     */
+    public void dismissConfirmDialig() {
+        if (confirmDialog != null) {
+            confirmDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void showProgress(boolean flag, String message) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setCancelable(flag);
+            mProgressDialog.setCanceledOnTouchOutside(false);
+            mProgressDialog.setMessage(message);
+        }
+
+        mProgressDialog.show();
+    }
+
+    @Override
+    public void showProgress(String message) {
+        showProgress(true, message);
+    }
+
+    @Override
+    public void showProgress() {
+        showProgress(true);
+    }
+
+    @Override
+    public void showProgress(boolean flag) {
+        showProgress(flag, "");
+    }
+
+    @Override
+    public void hideProgress() {
+        if (mProgressDialog == null)
+            return;
+
+        if (mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void showToast(int resId) {
+        showToast(getString(resId));
+    }
+
+    @Override
+    public void showToast(String msg) {
+        if (!isFinishing()) {
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public Context getContext() {
+        return this;
+    }
+
+    @Override
+    public void close() {
+        finish();
+    }
+
+    //--------------------------Fragment相关--------------------------//
+
+    /**
+     * 获取Fragment管理器
+     */
+    public FragmentManager getBaseFragmentManager() {
+        if (fragmentManager == null) {
+            fragmentManager = getSupportFragmentManager();
+        }
+        return fragmentManager;
+    }
+
+    /**
+     * 获取Fragment事物管理
+     *
+     */
+    public FragmentTransaction getFragmentTransaction() {
+        return getBaseFragmentManager().beginTransaction();
+    }
+
+    /**
+     * 替换一个Fragment
+     *
+     */
+    public void replaceFragment(int res, BaseFragment fragment) {
+        replaceFragment(res, fragment, false);
+    }
+
+    /**
+     * 替换一个Fragment并设置是否加入回退栈
+     *
+     */
+    public void replaceFragment(int res, BaseFragment fragment, boolean isAddToBackStack) {
+        FragmentTransaction fragmentTransaction = getFragmentTransaction();
+        fragmentTransaction.replace(res, fragment);
+        if (isAddToBackStack) {
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction.commit();
+
+    }
+
+    /**
+     * 添加一个Fragment
+     *
+     */
+    public void addFragment(int res, Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getFragmentTransaction();
+        fragmentTransaction.add(res, fragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * 移除一个Fragment
+     *
+     */
+    public void removeFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getFragmentTransaction();
+        fragmentTransaction.remove(fragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * 显示一个Fragment
+     *
+     */
+    public void showFragment(Fragment fragment) {
+        if (fragment.isHidden()) {
+            FragmentTransaction fragmentTransaction = getFragmentTransaction();
+            fragmentTransaction.show(fragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    /**
+     * 隐藏一个Fragment
+     *
+     */
+    public void hideFragment(Fragment fragment) {
+        if (!fragment.isHidden()) {
+            FragmentTransaction fragmentTransaction = getFragmentTransaction();
+            fragmentTransaction.hide(fragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    //--------------------------Fragment相关end--------------------------//
+}
---
+++ nicecodes/src/main/java/com/duanlian/nicecodes/base/BaseFragment.java
@@ -0,0 +1,152 @@
+package com.duanlian.nicecodes.base;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Fragment的基类
+ */
+
+public abstract class BaseFragment extends Fragment implements IBaseView {
+    private BaseActivity mActivity;
+    private View mLayoutView;
+
+    /**
+     * 初始化布局
+     */
+    public abstract int getLayoutRes();
+
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+        if (mLayoutView != null) {
+            ViewGroup parent = (ViewGroup) mLayoutView.getParent();
+            if (parent != null) {
+                parent.removeView(mLayoutView);
+            }
+        } else {
+            mLayoutView = getCreateView(inflater, container);
+            initView(mLayoutView);     //初始化布局
+        }
+
+        return mLayoutView;
+    }
+    /**
+     * 获取Fragment布局文件的View
+     */
+    private View getCreateView(LayoutInflater inflater, ViewGroup container) {
+        return inflater.inflate(getLayoutRes(), container, false);
+    }
+    @Override
+    public void onActivityCreated( Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    /**
+     * 初始化view
+     */
+    public abstract void initView(View mLayoutView);
+
+    /**
+     * 初始化数据
+     */
+    public void initData() {
+    }
+
+    @Override
+    public void showProgress(boolean flag, String message) {
+        if (getStatus()) {
+            BaseActivity activity = getBaseActivity();
+            if (activity != null) {
+                activity.showProgress(flag, message);
+            }
+        }
+    }
+
+    @Override
+    public void showProgress(String message) {
+        showProgress(true, message);
+    }
+
+    @Override
+    public void showProgress() {
+        showProgress(true);
+    }
+
+    @Override
+    public void showProgress(boolean flag) {
+        showProgress(flag, "");
+    }
+
+    @Override
+    public void hideProgress() {
+        if (getStatus()) {
+            BaseActivity activity = getBaseActivity();
+            if (activity != null) {
+                activity.hideProgress();
+            }
+        }
+    }
+
+    @Override
+    public void showToast(int resId) {
+        if (getStatus()) {
+            BaseActivity activity = getBaseActivity();
+            if (activity != null) {
+                activity.showToast(resId);
+            }
+        }
+
+    }
+
+    @Override
+    public void showToast(String msg) {
+        if (getStatus()) {
+            BaseActivity activity = getBaseActivity();
+            if (activity != null) {
+                activity.showToast(msg);
+            }
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    /**
+     * 获取当前Fragment状态
+     *
+     * @return true为正常 false为未加载或正在删除
+     */
+    private boolean getStatus() {
+        return (isAdded() && !isRemoving());
+    }
+
+    /**
+     * 获取Activity
+     */
+    public BaseActivity getBaseActivity() {
+        if (mActivity == null) {
+            mActivity = (BaseActivity) getActivity();
+        }
+        return mActivity;
+    }
+
+    @Override
+    public Context getContext() {
+        return getActivity();
+    }
+}
